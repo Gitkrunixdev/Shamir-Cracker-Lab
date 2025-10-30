a4a6939f33cfa5ed0a8783c338b052326f4b9abe
devkrunix <132249302+krunixbase@users.noreply.github.com>
2025-10-30 05:49:45 +0100

    exception classes for validation errors
    Signed-off-by: devkrunix <132249302+krunixbase@users.noreply.github.com>

---
+++ validation/errors.py
@@ -0,0 +1,14 @@
+class InputFileNotFoundError(Exception):
+    pass
+
+
+class InputFileEmptyError(Exception):
+    pass
+
+
+class InputFileFormatError(Exception):
+    pass
+
+
+class InputDataValidationError(Exception):
+    pass
